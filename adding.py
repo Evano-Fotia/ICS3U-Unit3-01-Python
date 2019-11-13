0bb2ee1f494a17287d04e5b92d7f606cfd52c1de
Ubuntu <ubuntu@ip-172-31-28-26.ec2.internal>
2019-11-13 19:02:27 +0000

    Initial commit

---
+++ adding.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by Evano Fotia
+# Created on Sept 2019
+# finding radius
+
+
+def main():
+    # This function adds two together
+
+    # Input
+    first_integer = int(input("Enter the first number to add (integer): "))
+    second_integer = int(input("Enter the second number to add (integer): "))
+
+    # Process
+    sum_ = first_integer + second_integer
+
+    # Output
+    print("")
+    print("The sum of your two integers is ", sum_)
+    print("(", first_integer, " + ", second_integer, " = ", sum_, ")")
+
+
+if __name__ == "__main__":
+    main()
